c75f55efc6edc0acad52c5d0af5d29700776fc75
SamuelAmiaka <amiakasamuel010@gmail.com>
2023-06-27 10:08:20 -0700

    In this file, import uploadPhoto and createUser from utils.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,5 @@
 /* eslint-disable*/
 import { uploadPhoto, createUser } from './utils.js';
-
 export default function handleProfileSignup() {
 	return Promise.all([uploadPhoto(), createUser()])
 	.then((result) => { 
